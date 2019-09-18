5d08f131e00343775803c5255046a85ab0d41aaa
Athena <43275423+simmingcat@users.noreply.github.com>
2019-09-18 13:49:40 -0600

    hihihihihihih

---
+++ New folder/childPage/scripts/introLesson/variables.js
@@ -0,0 +1,18 @@
+let firstName = "Athena";
+let lastName = "";
+let age = 0 "";
+
+let favouriteNumber = "8";
+let hair = "true";
+let male = "false";
+
+let female = "True";
+let favouriteGame = "OverWatch";
+let favouriteWord = "Potato";
+
+console.log('firstName');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
