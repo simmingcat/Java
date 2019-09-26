cc7be094ee7d6c252416b70fd84b262238f34513
Athena <43275423+simmingcat@users.noreply.github.com>
2019-09-26 13:30:20 -0600

    j

--- New folder/childPage/scripts/portrait.js
+++ New folder/childPage/scripts/portrait.js
@@ -1,2 +1,25 @@
-console.log("hello world")
-console.log("potato")
+let stop = false;
+
+function testNaN(number){
+console.log(stop);
+if (isNaN (number) ) {
+  stop = true;
+  return "type a real number"
+}else{
+  return "input validated"
+
+}
+}
+
+function main() {
+ let firstNumber, secondNumber, text;
+
+ firstNumber = document.getElementByID("textfield1").value;
+document.getElementByID("validityTest1").innerHTML = alert (testNaN (firstNumber));
+document.getElementByID("validityTest1").innerHTML = testNaN (firstNumber);
+
+ firstNumber = document.getElementByID("textfield2").value;
+document.getElementByID("validityTest2").innerHTML = alert (testNaN (firstNumber));
+document.getElementByID("validityTest2").innerHTML = testNaN (firstNumber);
+
+}
